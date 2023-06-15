2d75f892ebcd7d3cbbe9def9b2a6b628d33ea2b5
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-06-15 17:41:42 +0900

    php

--- 2022/exam22.php
+++ 2022/exam22.php
@@ -2,10 +2,18 @@
 $row = 1;
 //相対パス
 $path = "./2022/reports.csv";
+
+$idtxt = "./2022/id.txt";
+
+
 //空のレポート用配列
 $repo = array();
 //空のid配列
 $id = array();
+
+//txt側の配列
+$id2 = array();
+
 // ファイルが存在しているかチェックする
 if(file_exists($path)){
     //ファイルポインタ
@@ -13,32 +21,31 @@ if(file_exists($path)){
     if (($handle = $fp) !== FALSE) {
     // 1行ずつfgetcsv()関数を使って読み込む
         while (($data = fgetcsv($handle))) {
-            echo $row."行目\n";
+            echo $row."行目";
             //listにdataを配列化して代入
             $list = (array)$data;
             $id[$row] = $list[0];
+            $repo[$row] = $list[1];
 
-            if($list[1] == null){
-                $repo[$row]  = "【未提出】";
-            }else{
-                $repo[$row]  = $list[1];
-            }
-
-            echo $repo[$row].": ";
-            echo $id[$row]."\n";
+            //check 
+            echo $id[$row]." : ";
+            echo $repo[$row]."\n";
             $row++;
         }
         fclose($handle);
     }
     fclose($fp);
-
-    //htmlに<br>タグをつけて出力
-    // foreach($repo as $key => $value){
-    //     echo "$value<br/>\n";
-    // }
-    
-
 }else{
     echo "file not found";
+}
+
+$counter2 = 0;
+
+if(file_exists($idtxt)){
+    while(!feof($fp)){
+        $line = fgets($fp);
+        $id2 = explode(",",$line);
+        $counter2 ++ ;
+    }
 }
 ?>
