5441a37f8d1c7c168d025b2c64572e2d0e998311
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-06-15 12:52:55 +0900

    php

--- save.py
+++ save.py
@@ -2,4 +2,18 @@
 
 import os
 
-print(os.getcwd())
+
+def  check_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+input = raw_input("Enter the name of the directory to be created: ")
+
+check_dir(input)
+
+check_dir(input)
+
+print ("(^_^Directory created successfully")
+
+#check this directory
+
