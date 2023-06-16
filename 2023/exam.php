be1993a0c2a81bf336c895d2e34920f8fc09523e
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-06-16 09:34:35 +0900

    php

--- 2023/exam.php
+++ 2023/exam.php
@@ -1,9 +1,4 @@
-<html lang="ja">
-<head>
-    <title>前期中間試験</title>
-</head>
-<body>
-    <?php
+<?php
 
     $counter = 0;
 
@@ -15,7 +10,7 @@
 
     //ids.txt to $ Array
     if(file_exists($path0)){
-        // echo "File found";
+        echo "File found";
         $fp = fopen($path0, "r");
         while(!feof($fp)){
             $line = fgets($fp);
@@ -23,47 +18,51 @@
             $counter ++;
         }
     }else{
-        echo "File not found <br />\n";
+        echo "File not found\n";
     }
 
     $counter = 0;
 
     //sub.txt to $ Array
     if(file_exists($path1)){
-        // echo "File found";
+        echo "File found";
         $fp = fopen($path1, "r");
         while(!feof($fp)){
             $line = fgets($fp);
             $sub[$counter] = explode(",", $line);
 
-            //探索して指定されているかチェック
+            //線形探索
             for($i = 0 ; $i < 3 ; $i ++){
                 if($sub[$counter][$i] == ""){
                     $sub[$counter][$i] = "指定されていません";
                 }
             }
             $counter ++;
+
+            echo $sub[$counter-1][0]."\n";
         }
     }else{
-        echo "File not found <br/>\n";
+        echo "File not found\n";
     }
 
     //rename id to name
     $maxIndex = count($sub);
-    
+
     for ($i=0; $i < $maxIndex; $i++) { 
         $sub[$i][0] = $id[$i][1];
     }
 
-    // echo "done \n";
+    echo "done \n";
 
     //display
+
     foreach ($sub as $value) {
         echo $value[0].":";
         echo "[".$value[1]."] ";
         echo "[".$value[2]."] ";
-        echo "[".$value[3]."]<br/>\n";
+        echo "[".$value[3]."]\n";
     }
+
+
+    
 ?>
-</body>
-</html>
